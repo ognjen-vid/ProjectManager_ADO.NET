81dbb1141e39ba50ec81cddbe2883d151c879139
o <o>
2018-05-27 11:11:45 +0200

    Edit

--- ProjectManager/Models/Firm.cs
+++ ProjectManager/Models/Firm.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectManager.Models
 {
     public class Firm
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
 
         public List<Employee> Employees { get; set; } = new List<Employee>();
--- ProjectManager/ViewModel/ProjectViewModel.cs
+++ ProjectManager/ViewModel/ProjectViewModel.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using ProjectManager.Models; 
+using ProjectManager.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectManager.ViewModel
 {
